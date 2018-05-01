2f680622361383acb69b4aa597f0caefd5ac12b2
Akle2211 <37593615+Akle2211@users.noreply.github.com>
2018-05-01 10:49:03 +0200

    added comments

--- Snake_Game/Neural_Network_Function_Snake_Game.py
+++ Snake_Game/Neural_Network_Function_Snake_Game.py
@@ -19,14 +19,15 @@ import random
 def sigmoid_neg1_to_1(x):  # not going to define e
     return (2/(1 + 2.718281828459**-x)) - 1
 
+
 # Henrijs's sigmoid
 def sigmoid(x):  # not going to define e
     return 1/(1 + 2.718281828459**-x)
 
 
-# takes in a list of neurons(input_neurons), then create a list 
+# takes in a list of neurons(input_neurons), then create a list
 # of random integers the size of the list.
-# if input_neurons is instead a number, it will return a list 
+# if input_neurons is instead a number, it will return a list
 # the size of that number, full of random integers
 # the default random is -5,5, but it can be set very simply.
 # rand_list(3,-2,2) = [2,-1,0]
@@ -42,7 +43,7 @@ def rand_list(input_neurons, low=-5, high=5):
 
 # calculate the input array
 # this is an old function not to be used anymore.
-# it is better to directly put the input information directly in the 
+# it is better to directly put the input information directly in the
 # input array.
 # this function speak for itself though.
 def calc_input_arr(direction, closest, food, width, height='fool'):
@@ -55,15 +56,15 @@ def calc_input_arr(direction, closest, food, width, height='fool'):
             closest[3]/width,
             food[0]/width,
             food[1]/height,
-            1] #1 is the bias
+            1]  # 1 is the bias
     return temp
 
 
 # returns the next sets of neuron, the next layer.
-#zip can be used to walk 2 list at the same time (look link)
+# zip can be used to walk 2 list at the same time (look link)
 # https: // www.programiz.com/python-programming/methods/built-in/zip
-#calculate_layer basically calculate the hidden or the output layer of our neural network
-#it is used in the neural network function mainly
+# calculate_layer basically calculate the hidden or the output layer of our neural network
+# it is used in the neural network function mainly
 def calculate_layer(prev_layer, prev_weight_layer, bias=False):
     total = 0
     temp = []
@@ -88,20 +89,20 @@ def neural_network(input_array, weights_1, weights_2):
 
 
 # returns a layer of weights, depending on the neurons surrounding it
-# for example, if prev_layer = 3 and next_layer = 4, 
+# for example, if prev_layer = 3 and next_layer = 4,
 # create_weight_layer will return [[x,x,x],[x,x,x],[x,x,x],[x,x,x]]
 # x being random integers
 def create_weight_layer(prev_layer, next_layer):
     weights = []
-    for neuron in next_layer: # loops 4 times
-        weights.append(rand_list(prev_layer)) #create a list of 3 random integers
+    for neuron in next_layer:  # loops 4 times
+        weights.append(rand_list(prev_layer))  # create a list of 3 random integers
     return weights
 
-# takes the output array, and returns the direction, 
+
+# takes the output array, and returns the direction,
 # based on the max value, the highest probability
 def dir_of_neural_net(outputarray):
-    temp = outputarray.index(max(outputarray))
-    return temp
+    return outputarray.index(max(outputarray))
 
 
 # takes the values of a layer of neurons, and return their sigmoid.
@@ -110,13 +111,13 @@ def dir_of_neural_net(outputarray):
 def sigmoid_neurons(neurons):
     temp = []
     for neuron in neurons:
-        temp.append(sigmoid_neg1_to_1(neuron)) # change sigmoid here !!!
+        temp.append(sigmoid_neg1_to_1(neuron))  # change sigmoid here !!!
     return temp
 
 
 # this function was created by Henrijs, and i cannot define iteasily without mistake.
-#it is supposed to randomly swap the weights from 2 different neural network (the parents)
-#and then returns the child.
+# it is supposed to randomly swap the weights from 2 different neural network (the parents)
+# and then returns the child.
 def swap(array1, array2, first):  # array1 is the fittest network (first is there to check which array to give back)(17 subarray (including fitness score)
     if first == 1:
         for i in range(15):
@@ -130,3 +131,8 @@ def swap(array1, array2, first):  # array1 is the fittest network (first is ther
             # take weight from array two and assign it to array1 (1 => 2)
             array2[i+1][random_num] = array1[i+1][random_num]
         return array2
+
+
+# calculate the fitness of a neural network !
+def calc_fit(timefrombeginning, gamespeed, score):
+    return timefrombeginning*gamespeed + score
